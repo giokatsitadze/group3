9c7e9db9c34e9b88496e2b3c6d493649d9c70275
giokatsitadze <giokatsitadze20@gmail.com>
2023-03-17 12:49:03 +0400

    day3 commit

--- day3/lesson3.py
+++ day3/lesson3.py
@@ -1,1 +1,31 @@
-print("this is lesson 3")
+#name = ("gio")
+
+#print(len(name))
+full_name ="gio katsitadze"
+#print(len(full_name ))
+
+#print(full_name[0])
+
+#print("j" in full_name)
+
+
+full_name ="gio katsitadze"
+print(full_name[2:9])
+print(full_name[2:7:2])
+
+print(full_name[:5])
+print(full_name[5:])
+
+
+print(full_name[-1]+"abc")
+
+
+print(full_name.upper())
+
+name2 ="   NIKA   "
+print(name2.lower())
+print(name2.strip())
+
+print(name2.replace(" ","#"))
+
+
